ed60af66fe24f5b3088e4dba319c5f5b22b63c20
allen <codeday@qq.com>
2012-09-24 22:58:52 +0800

    change lib to phonegap 2.1

---
+++ assets/www/js/base/base.js
@@ -0,0 +1,38 @@
+
+function readfile(file){
+    
+    var reader = new FileReader();
+    
+    
+    
+}
+
+
+function getFile(uri){
+    window.resolveLocalFileSystemURI("file:///example.txt", onSuccess, onError);
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
