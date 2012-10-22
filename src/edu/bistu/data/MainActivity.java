4bb1cc756b2f9c9cafa3e19cfdae7dfc373d63d5
allen <codeday@qq.com>
2012-10-22 13:26:02 +0800

    ready to merge android

---
+++ src/edu/bistu/data/MainActivity.java
@@ -0,0 +1,18 @@
+package edu.bistu.data;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        super.setIntegerProperty("loadUrlTimeoutValue", 60000);
+//        super.setIntegerProperty("splashscreen",R.drawable.load);
+        super.loadUrl("file:///android_asset/www/index.html");
+        
+    }
+	
+}
