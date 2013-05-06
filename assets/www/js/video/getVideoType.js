72330cc6454fd9b8d1dd6e951ae95a2623ffaeaf
Allen Heavey <ista.allen@gmail.com>
2013-05-06 08:52:10 +0800

    bug record, special screen width can't load video and images

--- assets/www/js/video/getVideoType.js
+++ assets/www/js/video/getVideoType.js
@@ -1,9 +1,10 @@
 (function() {
 
-	var width = screen.width, moduleId = 5, drives = "android" + width, xhr = new XMLHttpRequest(), url = "http://m.bistu.edu.cn/api/api.php?table=moduletype&moduleid=5&drives=" + drives;
-	// http://m.bistu.edu.cn/api/api.php?table=moduletype&moduleid=5&drives=android480
-
-	Bistu.closeAble = false;
+	var width = screen.width
+	    , moduleId = 5
+	    , drives = "android" + width
+	    , xhr = new XMLHttpRequest()
+	    , url = "http://m.bistu.edu.cn/api/api.php?table=moduletype&moduleid=5&drives=" + drives;
 
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
@@ -44,28 +45,6 @@
 	
 	xhr.open("GET", url);
     xhr.send(null);
-	// $.get(url, null, function(results) {
-		// var typename, id, len = results.length, list = "";
-// 
-		// for (var i = 0; i < len; i++) {
-// 
-			// list += "<li data-icon=\"false\" title='" + results[i].id + "'><a href='videoList.html'>" + "<img src='" + results[i].coverpath + "'/><h3 class='vertical_m'>" + results[i].typename + "</h3><a/></li>";
-// 
-		// }
-// 
-		// $("#videoTypeContent").html(list);
-		// $("#videoTypeContent").listview('refresh');
-// 
-		// // set album type id for next page.
-		// $("#videoTypeContent li").each(function(index) {
-			// var typeid = $(this).attr("title");
-			// $(this).click(function() {
-				// window.localStorage.removeItem("videoTypeId");
-				// window.localStorage.setItem("videoTypeId", typeid);
-				// console.log("To video typeId-->" + typeid);
-			// });
-		// });
-	// }, "JSON");
 
 })();
 
